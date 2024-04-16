0cb19ccf4538051e0c0e8fa3f85679b9cf567d33
Maruyama Kohei <c0b23128fb@edu.teu.ac.jp>
2024-04-16 17:30:51 +0900

    工科トン移動演習1

--- pygame_image.py
+++ pygame_image.py
@@ -28,7 +28,8 @@ def main():
         elif key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         elif key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((2, 0))
+            
 
         x = tmr%1600
         screen.blit(bg_img, [-x, 0])
